cdbb8894e1c48d39bb76787ffbce4dbe3728f92c
HECHLJ <johannes.hechler@ons.gov.uk>
2021-03-09 16:39:47 +0000

    made scripts needed by Jenkins, probably incomplete

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='adruk_tools',
+    version='0.0.1',
+    description='collection of pyspark functions written by the ADRUK team for data engineering',
+    url='http://np2rvlapxx507/hechlj/',
+  author = 'Sophie-Louise Courtney, Johannes Hechler, Ben Marshall-Sheen'
+    packages = find_packages()
+)
