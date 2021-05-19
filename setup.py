5f40a90f30e30925999d0f743e70974e586903a8
HECHLJ <johannes.hechler@ons.gov.uk>
2021-05-19 15:45:34 +0000

    made new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='adruk_tools',
-    version='0.0.',
+    version='0.0.2',
     description='collection of pyspark functions written by the ADRUK team for data engineering',
     #url='http://np2rvlapxx507/hechlj/',
   author = 'Sophie-Louise Courtney, Johannes Hechler, Ben Marshall-Sheen',
