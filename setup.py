b7628fc3f58a2c7b57f3fcaa61c9f46f757d3697
Diogo Marques <diogo.de.oliveira.marques@ons.gov.uk>
2021-07-09 16:35:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     description='collection of pyspark functions written by the ADRUK team for data engineering',
     url='http://np2rvlapxx507/data_engineering/de-utils.git',
     author = 'Sophie-Louise Courtney, Johannes Hechler, Ben Marshall-Sheen',
-    packages = ['adruk_tools.functions'],
+    packages = ['adruk_tools','adruk_tools.functions'],
     install_requires=requirements
 )
