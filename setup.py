f9a304c0b028e04ae25359e3acbdef08691e8416
Johannes Hechler <johannes.hechler@ons.gov.uk>
2021-07-09 16:10:53 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='adruk_tools',
+    name='functions',
     version=__version__,
     description='collection of pyspark functions written by the ADRUK team for data engineering',
     url='http://np2rvlapxx507/data_engineering/de-utils.git',
