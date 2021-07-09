e3dca05d5d6d41b18a70e13c9b86e5fc716e50e6
HECHLJ <johannes.hechler@ons.gov.uk>
2021-07-09 11:08:02 +0000

    add version file and edit setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+from de_utils._version import __version__
 
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='adruk_tools',
-    version='0.0.2',
+    version=__version__,
     description='collection of pyspark functions written by the ADRUK team for data engineering',
-    #url='http://np2rvlapxx507/hechlj/',
+    url='http://np2rvlapxx507/data_engineering/de-utils.git',
     author = 'Sophie-Louise Courtney, Johannes Hechler, Ben Marshall-Sheen',
-    packages = 'functions'
+    packages = ['adruk_tools', 'adruk_tools.functions'],
+    install_requires=requirements
+  
 )
