92bb1d8279656722d30a696bea64ca3bea1c4fd6
Johannes Hechler <johannes.hechler@ons.gov.uk>
2021-09-07 12:12:00 +0100

    Revert "Merge branch 'setup' into 'master'"
    This reverts merge request !31

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup
 from adruk_tools._version import __version__
 
 
-with open('adruk_tools/requirements.txt') as f:
+with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name = 'adruk_tools',
-    version = __version__,
-    description = 'collection of pyspark functions written by the ADRUK team for data engineering',
-    url = 'http://np2rvlapxx507/data_engineering/de-utils.git',
+    name='adruk_tools',
+    version=__version__,
+    description='collection of pyspark functions written by the ADRUK team for data engineering',
+    url='http://np2rvlapxx507/data_engineering/de-utils.git',
     author = 'Sophie-Louise Courtney, Johannes Hechler, Ben Marshall-Sheen',
     packages = ['adruk_tools','adruk_tools.functions'],
-    install_requires = requirements
+    install_requires=requirements
 )
