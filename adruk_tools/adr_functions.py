d4a9e63735fb97047c66619cfdf9cd04f7af3ed3
Johannes Hechler <johannes.hechler@ons.gov.uk>
2022-02-22 13:36:11 +0000

    updated generate_ids docstring for discovered behaviour of start_year parameter

--- adruk_tools/adr_functions.py
+++ adruk_tools/adr_functions.py
@@ -687,50 +687,119 @@ def make_test_df(session_name):
 def generate_ids(session, df, id_cols, start_year, id_len = None):
   """
   :WHAT IT IS: pyspark function
-  :WHAT ID DOES: recodes a set of columns to random numerical values
+  :WHAT IT DOES: recodes a column, or combinations of columns, into a unique random numerical values.
+  
+  Creates unique random numerical values for all unique combinations (permutations) of columns entered under 
+  id_cols and start_year, with start_year added as a prefix to the generated numerical 
+  values. Output is a new table with columns under id_cols, allowing a join back to the original dataset, 
+  and the new numerical id. Additional option to create numerical id of specified length.
+
   :WHY IT DOES IT: to anonymise ID variables in ADRUK projects
-  :RETURNS: dataframe with 2 columns, mapping old and new IDs
-  * old ID (unique values only)
+  
+  :RETURNS: dataframe with 2 or more columns, mapping old and new IDs
+  * old ID (unique combinations of columns in id_cols and start_year, displayed in seperate columns)
   * new ID (unique values only) , called 'adr_id'
+  
   :OUTPUT VARIABLE TYPE: spark dataframe. new ID column = string type
   
   :AUTHOR: hard-coded by David Cobbledick, function by Johannes Hechler
   :DATE: 2020
   :VERSION: 0.0.1
-  :KNOWN ISSUES: input dataset must not have existing column called 'adr_id'
+  :KNOWN ISSUES:
+    * input dataset must not have existing column called 'adr_id'
+    * the parameter 'start_year' is actually used as part of the parameter 
+    'id_cols'. That means that the function distinguishes not just distinct 
+    values of the id columns, but distinct combinations of id columns and 
+    start_year. If you don't want this start_year must be a column with only
+    1 value. start_year is a compulsory parameter, so if you only want to 
+    distinguish distinct values of the id_cols then create an auxiliary 
+    column with just 1 distinct value to use as start_year.
 
   :PARAMETERS:
     * session = name of active spark cluster
       `(datatype = cluster name, no string)`, e.g. spark
+      
     * df = spark dataframe with ID you want derive random IDs from
       `(datatype = dataframe name, no string)`, e.g. PDS
+      
     * id_cols = column(s) to turn into randomised new ID
       `(datatype = list of strings)`, e.g. ['year', 'name']
-    * start_year = name of additional column to prefix (in the clear) to the random IDs. NB if several columns are supplied, only the first is used.
+      
+    * start_year = name of additional column to prefix (in the clear) to 
+      the anonymised ids.
+      NB if several columns are supplied, only the first is used.
+      NB this column is actually used with the id_cols, i.e. if start_year has
+      several distinct values it potentially inflates the number of ids identified
+      by the function
       `(datatype = list of strings)`, e.g. ['year']
+      
     * id_len = set uniform length of ID values (ignoring length of start_year values) if required. Pads out values with leading zeroes if needed. Default value = None, i.e. accept different lengths
       `(datatype = numeric)`, e.g. 9
 
-  :EXAMPLE:
+
+
+  :EXAMPLES:
+  
+  :SAMPLE INPUT:
+  
+  +-----+---+----+
+  | name| ID| age|
+  +-----+---+----+
+  | John|AA3|  23|
+  |  Tom|AA8|  32|
+  |Alice|AA4|  44|
+  | John|AA3|  61|
+  |  Tom|AA8|  32|
+  |Alice|AA4|  44|
+  +-----+---+----+
+
   >>> generate_ids(sessions = spark, 
                     df = AEDE, 
-                    id_cols = ['name', 'ID'],
-                    start_year = ['year'], 
+                    id_cols = ['name'],
+                    start_year = ['age'], 
                     id_len = 9)
   
   :SAMPLE OUTPUT:
-  +-----+---+-------------+
-  | name| ID|       adr_id|
-  +-----+---+-------------+
-  | john|AA3|2019782853507|
-  |  Tom|AA8|2019659404170|
-  |Alice|AA1|2019145833675|
-  | Matt|AA6|2019485000031|
-  |Linda|AA2|2019156515405|
-  |Susan|AA4|2019621073989|
-  +-----+---+-------------+
-  """
+  +-----+-----------+
+  | name|     adr_id|
+  +-----+-----------+
+  |Alice|44277926008|
+  | John|23534343875|
+  | John|61534343875|
+  |  Tom|32580421606|
+  +-----+-----------+
   
+   :SAMPLE INPUT:
+  
+  +-----+---+----+
+  | name| ID| age|
+  +-----+---+----+
+  | John|AA3|  23|
+  |  Tom|AA8|  32|
+  |Alice|AA1|  44|
+  | Matt|AA6|  61|
+  |  Tom|AA8|  32|
+  |Alice|AA4|  44|
+  +-----+---+----+
+
+  >>> generate_ids(sessions = spark, 
+                    df = AEDE, 
+                    id_cols = ['name', 'ID'],
+                    start_year = ['age'], 
+                    id_len = 9)
+  
+  :SAMPLE OUTPUT:
+  +-----+---+-----------+
+  | name| ID|     adr_id|
+  +-----+---+-----------+
+  |  Tom|AA8|32974159768|
+  |Alice|AA1|44074421659|
+  |Alice|AA4|44248294836|
+  | Matt|AA6|61683839080|
+  | John|AA3|23683839080|
+  +-----+---+-----------+
+  """
+
   
   #==========================================================================
   """LOAD REQUIRED PACKAGES"""
