04cf53d6f32cf008e7bb19da671a9bb19c11421a
shawn1 <Nathan.Shaw@ons.gov.uk>
2022-02-22 11:41:49 +0000

    Update comments in conftest

--- conftest.py
+++ conftest.py
@@ -1,24 +1,30 @@
 """
 WHAT IT IS: PySpark (python) script
-WHAT IT DOES: pipeline to complete easy_pipeline task. JIRA ticket ONSDS-3235
+WHAT IT DOES: Stanard pytest file so that tests from multiple modules in the directory can access
+the fixture function. Creates a single spark session for all tests (scripts starting with test_) and
+when all of them have been run, the session is closed. This script needs to sit in the root of the
+working directory.
 AUTHOR: Nathan Shaw
-CREATED: 02/02/2022
-LAST UPDATE: 
+CREATED: 19/02/2022
+LAST UPDATE: 22/02/2022 
 """
 
 from pyspark.sql import SparkSession
-
 import pytest
 
-
+# The pytest fixture prepares, and manages, the environment for testing - in this case create and stopping
+# a spark session.
+# To ensure the spark session is visible for all tests accross many modules, scope variable is set to session
+# implying the spark session stays open for the entirity of the test.
+# See here for further info: https://docs.pytest.org/en/6.2.x/fixture.html#funcargs
 @pytest.fixture(scope='session')
 def spark_context():
   
     spark = (
-    SparkSession.builder.appName("harder_pipeline_nathan")
-    .config("spark.sql.shuffle.partitions", 10)
-    .enableHiveSupport()
-    .getOrCreate()
+      SparkSession.builder.appName("adruk_tests")
+      .config("spark.sql.shuffle.partitions", 10)
+      .enableHiveSupport()
+      .getOrCreate()
     )
     
     yield spark
