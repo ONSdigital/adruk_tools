6b7cbca073bb756c305f7def94ceaf96a037d09b
shawn1 <Nathan.Shaw@ons.gov.uk>
2022-02-22 14:25:16 +0000

    update docstrings and comments in unit tests

--- tests/test_generate_ids.py
+++ tests/test_generate_ids.py
@@ -1,172 +1,226 @@
-# https://www.sicara.ai/blog/2019-01-14-tutorial-test-pyspark-project-pytest
+"""
+WHAT IT IS: python script
+WHAT IT DOES: Runs a set of unit tests on the generate_ids() function in adr_functions.py
+AUTHOR: Nathan Shaw
+CREATED: 19/02/2022
+LAST UPDATE: 22/02/2022
 
-# turn into panadas and checks can be undertaken using assert calls
-# https://pandas.pydata.org/docs/reference/general_utility_functions.html#testing-functions
+"""
+from importlib.machinery import SourceFileLoader
 
-
-# https://github.com/MrPowers/chispa
-
-# Could use chispa for pyspark testing. Looks a useful package.
-# blog post here https://mungingdata.com/pyspark/testing-pytest-chispa/
-
-#from adruk_tools import adr_functions as adr
-import pyspark.sql.types as T
 import pandas as pd
 import pytest as pt
 
 
-#######################################
-from importlib.machinery import SourceFileLoader
-
+# Provide explicit file path to updated function, otherwise the old version in the package is referenced 
+# At least was for me
 adr = SourceFileLoader("adr_functions", "/home/cdsw/adruk_tools/adruk_tools/adr_functions.py").load_module()
-#######################################
 
-# Test data
+
+# Create a test dataset to be used throughout the tests. This is intially created in Spark
+# so the function can run, before being converted to pandas to undertake assertation.
 
 test_columns = ['name', 'id', 'age', 'bmi', 'year']
 
-test_rows = [('Nathan', 'A', 23, 45.679, '2008'), 
-     ('Joanna', 'B', 63, 25.080, '2008'), 
+test_rows = [('Nathan', 'A', 23, 45.679, '2008'),
+     ('Joanna', 'B', 63, 25.080, '2008'),
      ('Tom', 'A', 89, 99.056, '2008'),
-     ('Nathan', 'C', 23, 45.679, '2008'), # same as row 1
-     ('Nathan', 'A', 23, 45.679, '2008'),# same as row 1, different id 
-     ('Johannes', 'E', 67, 25.679, '2009'), # new year
-     ('Nathan', 'B', 23, 45.679, '2009'), # same as row 1, different year
-     ('Johannes', 'E', 67, 45.679, '2009'), # same as row 1, different id, different year
-     ('Nathan', None, 23, 45.679, '2009'), # test empty id
-     ('Nathan', 'C', 23, 45.679, None), # test empty year
+     ('Nathan', 'C', 23, 45.679, '2008'),
+     ('Nathan', 'A', 23, 45.679, '2008'),
+     ('Johannes', 'E', 67, 25.679, '2009'),
+     ('Nathan', 'B', 23, 45.679, '2009'),
+     ('Johannes', 'E', 67, 45.679, '2009'),
+     ('Nathan', None, 23, 45.679, '2009'),
+     ('Nathan', 'C', 23, 45.679, None),
      (None, 'F', 89, 99.056, '2008')]
 
+# Tests have been created to check two things for a variety of input parameters
+# 1 - Check that the created adr_id column is unique
+# 2 - Number of unique values match between adr_id and unique permutations of
+# other input columns (from id_cols and start_year).
 
+# These two tests are undertaken for the following input parameter scenarios
+# a - 1 x id_col, 0 x start_year
+# b - 2 x id_col, 0 x start_year
+# c - 1 x id_col, 1 x start_year
+# d - 2 x id_col, 1 x start_year
 
-
+# Note that all functions start with test_ and call in the spark_context created
+# in conftest.py. Requirements for the tests to be run.
 
 def test_one_col_adr_id_unique(spark_context):
-  
+  """
+  :WHAT IT IS: Python function
+  :WHAT IT DOES: tests generate_ids() function creates a unique adr_id column
+  in the case when one id_col is provided.
+  """
   
   input_dataset = spark_context.createDataFrame(test_rows, test_columns)
   
-  is_expected_output_unique = True
-  
   real_output = adr.generate_ids(spark_context, input_dataset, ['id']).toPandas()
-  
-  is_real_output_unique = real_output['adr_id'].nunique() == len(real_output['adr_id'])
-  
-  #== real_output['id'].count()
 
-  
-  print( "columns in the output table are not unique")
+  # Test whether generated adr_id column contains only unique values
+  is_real_output_unique = real_output['adr_id'].nunique() == len(real_output['adr_id'])
+
+  # Expected outcome of uniqueness test i.e. we expect adr_id to be True
+  is_expected_output_unique = True
+
+  # Test equality between expected and generated outcomes
+  print( "adr_id is not unique")
   assert is_expected_output_unique == is_real_output_unique
   
 
   
 def test_one_col_equal_count(spark_context):
-  # count between adr_id and combination of other columns match
-  
+  """
+  :WHAT IT IS: Python function
+  :WHAT IT DOES: tests that number of unique values are the same between adr_id
+  column and unique permutations of id_cols and start_year. In this case one id_col
+  variable.
+  """
   input_dataset = spark_context.createDataFrame(test_rows, test_columns)
-  
-  expected_output_count = 6
-  
+
   real_output = adr.generate_ids(spark_context, input_dataset, ['id']).toPandas()
   real_output_count = real_output['adr_id'].nunique()
- 
-  print("count of unique dont match between id and adr_id in output table")
+  
+  # Manually calculated number of unique values in permutations of id_cols and start_year
+  expected_output_count = 6
+  
+  # Test equality between expected and generated outcomes
+  print("count of unique values do not match between adr_id and input columns")
   assert expected_output_count == real_output_count
   
   
 
 def test_two_col_adr_id_unique(spark_context):
-  
-  
+  """
+  :WHAT IT IS: Python function
+  :WHAT IT DOES: tests generate_ids() function creates a unique adr_id column
+  in the case when two id_col are provided.
+  """
+
   input_dataset = spark_context.createDataFrame(test_rows, test_columns)
   
-  is_expected_output_unique = True
-  
   real_output = adr.generate_ids(spark_context, input_dataset, ['name', 'id']).toPandas()
-  
-  is_real_output_unique = real_output['adr_id'].nunique() == len(set(zip(real_output['name'], real_output['id'])))
 
-  
-  print( "columns in the output table are not unique")
+  # Test whether generated adr_id column contains only unique values
+  is_real_output_unique = real_output['adr_id'].nunique() == len(real_output['adr_id'])
+    
+  # Expected outcome of uniqueness test i.e. we expect adr_id to be True
+  is_expected_output_unique = True
+
+  # Test equality between expected and generated outcomes
+  print( "adr_id is not unique")
   assert is_expected_output_unique == is_real_output_unique
-  
+
 
   
 def test_two_col_equal_count(spark_context):
-  # count between adr_id and combination of other columns
+  """
+  :WHAT IT IS: Python function
+  :WHAT IT DOES: tests that number of unique values are the same between adr_id
+  column and unique permutations of id_cols and start_year. In this case two id_col
+  variables.
+  """
   
   input_dataset = spark_context.createDataFrame(test_rows, test_columns)
-  
-  expected_output_count = 8
-  
+
   real_output = adr.generate_ids(spark_context, input_dataset, ['name', 'id']).toPandas()
   real_output_count = real_output['adr_id'].nunique()
- 
-  print("count of unique dont match between id and adr_id in output table")
-  assert expected_output_count == real_output_count
-
-
-def test_one_col_start_year_adr_id_unique(spark_context):
   
-  
-  input_dataset = spark_context.createDataFrame(test_rows, test_columns)
-  
-  is_expected_output_unique = True
-  
-  real_output = adr.generate_ids(spark_context, input_dataset, ['id'], ['age']).toPandas()
-  
-  is_real_output_unique = real_output['adr_id'].nunique() == len(real_output['adr_id'])
-  
-  #== real_output['id'].count()
-
-  
-  print( "columns in the output table are not unique")
-  assert is_expected_output_unique == is_real_output_unique
-  
-
-  
-def test_one_col_start_year_equal_count(spark_context):
-  # count between adr_id and combination of other columns match
-  
-  input_dataset = spark_context.createDataFrame(test_rows, test_columns)
-  
+  # Manually calculated number of unique values in permutations of id_cols and start_year
   expected_output_count = 8
   
-  real_output = adr.generate_ids(spark_context, input_dataset, ['id'], ['age']).toPandas()
-  real_output_count = real_output['adr_id'].nunique()
- 
-  print("count of unique dont match between id and adr_id in output table")
+  # Test equality between expected and generated outcomes
+  print("count of unique values do not match between adr_id and input columns")
   assert expected_output_count == real_output_count
-  
-  
-  
-def test_two_col_start_year_adr_id_unique(spark_context):
-  
+    
+    
+
+def test_one_col_start_year_adr_id_unique(spark_context):
+  """
+  :WHAT IT IS: Python function
+  :WHAT IT DOES: tests generate_ids() function creates a unique adr_id column
+  in the case when one id_col is provided and one start_year.
+  """
   
   input_dataset = spark_context.createDataFrame(test_rows, test_columns)
   
+  real_output = adr.generate_ids(spark_context, input_dataset, ['id'], ['age']).toPandas()
+
+  # Test whether generated adr_id column contains only unique values
+  is_real_output_unique = real_output['adr_id'].nunique() == len(real_output['adr_id'])
+
+  # Expected outcome of uniqueness test i.e. we expect adr_id to be True
   is_expected_output_unique = True
+
+  # Test equality between expected and generated outcomes
+  print( "adr_id is not unique")
+  assert is_expected_output_unique == is_real_output_unique
+
+
+
+def test_one_col_start_year_equal_count(spark_context):
+  """
+  :WHAT IT IS: Python function
+  :WHAT IT DOES: tests that number of unique values are the same between adr_id
+  column and unique permutations of id_cols and start_year. In this case one id_col
+  variable and one start_year variable.
+  """
+  
+  input_dataset = spark_context.createDataFrame(test_rows, test_columns)
+
+  real_output = adr.generate_ids(spark_context, input_dataset, ['id'], ['age']).toPandas()
+  real_output_count = real_output['adr_id'].nunique()
+  
+  # Manually calculated number of unique values in permutations of id_cols and start_year
+  expected_output_count = 8
+  
+  # Test equality between expected and generated outcomes
+  print("count of unique values do not match between adr_id and input columns")
+  assert expected_output_count == real_output_count
+
+
+
+def test_two_col_start_year_adr_id_unique(spark_context):
+  """
+  :WHAT IT IS: Python function
+  :WHAT IT DOES: tests generate_ids() function creates a unique adr_id column
+  in the case when two id_col are provided and one start_year variable.
+  """
+
+  input_dataset = spark_context.createDataFrame(test_rows, test_columns)
   
   real_output = adr.generate_ids(spark_context, input_dataset, ['name', 'id'], ['age']).toPandas()
-  
-  is_real_output_unique = real_output['adr_id'].nunique() == len(set(zip(real_output['name'], real_output['id'])))
 
-  
-  print( "columns in the output table are not unique")
+  # Test whether generated adr_id column contains only unique values
+  is_real_output_unique = real_output['adr_id'].nunique() == len(real_output['adr_id'])
+    
+  # Expected outcome of uniqueness test i.e. we expect adr_id to be True
+  is_expected_output_unique = True
+
+  # Test equality between expected and generated outcomes
+  print( "adr_id is not unique")
   assert is_expected_output_unique == is_real_output_unique
-  
+
 
   
 def test_two_col_start_year_equal_count(spark_context):
-  # count between adr_id and combination of other columns
+  """
+  :WHAT IT IS: Python function
+  :WHAT IT DOES: tests that number of unique values are the same between adr_id
+  column and unique permutations of id_cols and start_year. In this case two id_col
+  variables and one start_year variable.
+  """
   
   input_dataset = spark_context.createDataFrame(test_rows, test_columns)
-  
-  expected_output_count = 8
-  
+
   real_output = adr.generate_ids(spark_context, input_dataset, ['name', 'id'], ['age']).toPandas()
   real_output_count = real_output['adr_id'].nunique()
- 
-  print("count of unique dont match between id and adr_id in output table")
-  assert expected_output_count == real_output_count
   
+  # Manually calculated number of unique values in permutations of id_cols and start_year
+  expected_output_count = 8
+  
+  # Test equality between expected and generated outcomes
+  print("count of unique values do not match between adr_id and input columns")
+  assert expected_output_count == real_output_count
