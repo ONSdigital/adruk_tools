780501f2f53fa8227689405a8fa3effc79ea2986
bardos <Silvia.Bardoni@ons.gov.uk>
2023-04-11 15:45:31 +0100

    improvements on test open_yaml

--- tests/test_open_yaml_config.py
+++ tests/test_open_yaml_config.py
@@ -7,62 +7,62 @@ LAST UPDATE: 24/02/2023
 
 """
 
-import adruk_tools.adr_functions as d
+import adruk_tools.adr_functions as adr
 import pytest
 import yaml
 
 
 class TestOpenYamlConfig(object):
-  
+
   """Unit tests for the function open_yaml_config. It tests errors, type and values"""
-  
- 
+
+
   yaml_file_path = "/home/cdsw/adruk_tools/tests/test_config.yaml"
   yaml_non_existing_file_path = "/home/cdsw/adruk_tools/tests/non_existing_file.yaml"
   yaml_errors_file_path = "/home/cdsw/adruk_tools/tests/test_config_yamlerror.yaml"
-  
-  
+
+
 
   ## TESTS
-  #---------  
+  #---------
 
-  
-  #1. Does the function raise an error when the input file does not exist?  
-  def test_open_file_config_error(self):     
+
+  #1. Does the function raise an error when the input file does not exist?
+  def test_open_file_not_exist(self):
     with pytest.raises(OSError):
-      d.open_yaml_config(self.yaml_non_existing_file_path) 
-  
+      adr.open_yaml_config(self.yaml_non_existing_file_path)
 
-  
-  #2 Does the function raise an error when the yaml file has syntax errors?  
-  def test_open_yaml_config_error(self):     
+
+
+  #2 Does the function raise an error when the yaml file has syntax errors?
+  def test_open_yaml_syntax_error(self):
     with pytest.raises(yaml.parser.ParserError):
-      d.open_yaml_config(self.yaml_errors_file_path) 
+      adr.open_yaml_config(self.yaml_errors_file_path)
 
-      
-  
-  #3. Does the function return a dictionary? 
+
+
+  #3. Does the function return a dictionary?
   def test_return_open_yaml_config(self):
-    assert isinstance(d.open_yaml_config(self.yaml_file_path),dict)
-  
-  
-  
-  #4. Is the dictionary empty (=empty yaml file)?  
+    assert isinstance(adr.open_yaml_config(self.yaml_file_path),dict)
+
+
+
+  #4. Is the dictionary empty (=empty yaml file)?
   def test_dictionary_open_yaml_config(self):
-    assert len(d.open_yaml_config(self.yaml_file_path)) != 0
-  
-  
-  
+    assert len(adr.open_yaml_config(self.yaml_file_path)) != 0
+
+
+
 
   #5. Test all the elements the dictionary and check that returns the correct values.
   # Please, go to the example yaml file (test_config.yaml) to check variables and
   # values.
   def test_values_open_yaml_config(self):
-    assert (d.open_yaml_config(self.yaml_file_path)["settings"]["file"]== "test/file/test.csv")
-    assert (d.open_yaml_config(self.yaml_file_path)["settings"]["columns"]== ["column_1","column_2","column_3"])
-    assert (d.open_yaml_config(self.yaml_file_path)["settings"]["test_data"]== True)
-    assert (d.open_yaml_config(self.yaml_file_path)["settings"]["number_of_columns"]== 3)
+    assert (adr.open_yaml_config(self.yaml_file_path)["settings"]["file"]== "test/file/test.csv")
+    assert (adr.open_yaml_config(self.yaml_file_path)["settings"]["columns"]== ["column_1","column_2","column_3"])
+    assert (adr.open_yaml_config(self.yaml_file_path)["settings"]["test_data"]== True)
+    assert (adr.open_yaml_config(self.yaml_file_path)["settings"]["number_of_columns"]== 3)
 
-  
-  
- 
+
+
+
